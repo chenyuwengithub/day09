5e540da1364091e340a359b75d13bb221f51381b
chenyuwengithub <550421089@qq.com>
2020-08-16 09:52:58 +0800

    day09代码

---
+++ ssm/src/main/java/com/xiaoshu/entity/Student.java
@@ -0,0 +1,108 @@
+package com.xiaoshu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Student implements Serializable {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+    
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date brithday;
+
+    private Integer age;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    /**
+     * @return sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex
+     */
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    /**
+     * @return brithday
+     */
+    public Date getBrithday() {
+        return brithday;
+    }
+
+    /**
+     * @param brithday
+     */
+    public void setBrithday(Date brithday) {
+        this.brithday = brithday;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", sex=").append(sex);
+        sb.append(", brithday=").append(brithday);
+        sb.append(", age=").append(age);
+        sb.append("]");
+        return sb.toString();
+    }
+}
